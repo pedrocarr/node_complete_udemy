357754eeccb300308e42545f7934f867c9b71f24
Pedro Carvalho <eng.pedro.amb@gmail.com>
2021-11-03 09:53:35 -0300

    docs: fix spelling of CONST

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const routes = require('./routes');
 
 console.log(routes.someText);
 
-consT server = http.createServer(routes.handler);
+const server = http.createServer(routes.handler);
 
 server.listen(3000, () => {
     console.log("SERVER IS RUNNING");
