fa100e9da18b2aeee7e05a0123c2a60cd2e09e3d
Pedro Carvalho <eng.pedro.amb@gmail.com>
2021-11-03 14:33:26 -0300

    chore: simplify express syntax

--- app.js
+++ app.js
@@ -3,11 +3,6 @@ const express = require('express');
 const app = express();
 
 app.use((req, res, next) => {
-    console.log("In this middleware!");
-    next();
-})
-
-app.use((req, res, next) => {
     console.log("In other middleware!");
     res.send('<h1>Hello from Express!</h1>');
 })
