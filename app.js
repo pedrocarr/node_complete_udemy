2c23828a9f54049d6e4e3afc333cdd25a0f0ab16
Pedro Carvalho <eng.pedro.amb@gmail.com>
2021-11-04 14:13:45 -0300

    chore: add body-parser

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.use('/add-product', (req, res, next) => {
-    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit"')
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Send</button></form>')
 });
+
+app.use('/product', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/');
+})
 
 
 app.use('/', (req, res, next) => {
-    console.log("In other middleware!");
     res.send('<h1>Hello from Express!</h1>');
 })
 
