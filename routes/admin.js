943b6c5e64829736248bdd4325285bbf50be0068
Pedro Carvalho <eng.pedro.amb@gmail.com>
2021-11-04 15:16:27 -0300

    chore: add views to the project

--- routes/admin.js
+++ routes/admin.js
@@ -4,7 +4,7 @@ const router = express.Router();
 
 
 router.get('/add-product', (req, res, next) => {
-    res.send('<form action="/add-product" method="POST"><input type="text" name="title"><button type="submit">Send</button></form>')
+    res.send('<form action="/admin/add-product" method="POST"><input type="text" name="title"><button type="submit">Send</button></form>')
 });
 
 router.post('/add-product', (req, res, next) => {
