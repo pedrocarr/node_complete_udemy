e3e8400238ad0e617a506c1ef4bc7c69023d264a
Pedro Carvalho <eng.pedro.amb@gmail.com>
2021-11-04 09:54:42 -0300

    chore: add routes to the app

--- app.js
+++ app.js
@@ -2,7 +2,13 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
+
+app.use('/add-product', (req, res, next) => {
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit"')
+});
+
+
+app.use('/', (req, res, next) => {
     console.log("In other middleware!");
     res.send('<h1>Hello from Express!</h1>');
 })
